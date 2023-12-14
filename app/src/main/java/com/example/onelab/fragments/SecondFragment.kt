9ec2d4479e99d9024be0e910069ec89979835f20
ShakirKadirov <shakirkadirov1@gmail.com>
2023-12-15 03:04:14 +0600

    add to project to Fragment

---
+++ app/src/main/java/com/example/onelab/fragments/SecondFragment.kt
@@ -0,0 +1,13 @@
+package com.example.onelab.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.onelab.R
+
+
+class SecondFragment : Fragment(R.layout.fragment_second) {
+
+}
