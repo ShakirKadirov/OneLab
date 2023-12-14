129b51f70b8df4a2dfb96dc99500ff1130172c97
ShakirKadirov <shakirkadirov1@gmail.com>
2023-12-15 02:37:37 +0600

    add TextView to SecondActivity

--- app/src/main/java/com/example/onelab/SecondActivity.kt
+++ app/src/main/java/com/example/onelab/SecondActivity.kt
@@ -1,11 +1,44 @@
 package com.example.onelab
 
+import android.annotation.SuppressLint
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
+
+    private var counter = 0
+    private lateinit var counterTextView: TextView
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        if (savedInstanceState != null) {
+            counter = savedInstanceState.getInt("counter")
+        }
+
+        counterTextView = findViewById(R.id.counterTextView)
+
+        updateCounter()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putInt("counter", counter)
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+
+        counter++
+
+        updateCounter()
+    }
+
+    private fun updateCounter() {
+        counterTextView.text = counter.toString()
     }
 }
